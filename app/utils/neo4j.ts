1be878e9aa394d77957ad63a240f864c92ce9349
arielshulman <ariel@illumex.ai>
2024-05-14 15:17:17 +0300

    add: extract neo4j results by key method

--- app/utils/neo4j.ts
+++ app/utils/neo4j.ts
@@ -42,6 +42,24 @@ export const extractResultsObjectFromNeo4jRecords = <ItemType>(
   const validatedResult = schema.safeParse(result);
   if (validatedResult.success) {
     return validatedResult.data;
+export const extractResultsObjectFromNeo4jRecordsbyKey = <ItemType>(
+  records: any,
+  recordsKeyToValidate: string,
+  schema: z.Schema<ItemType>
+): z.SafeParseReturnType<ItemType, ItemType> => {
+  const typedRecords = neo4jObjectRecordSchema.safeParse(records);
+  if (!typedRecords.success) {
+    throw new Error("Error in validating data");
+  }
+  const result: Record<string, unknown> = {};
+  const { _fields: values, _fieldLookup: keysToValueIndex } =
+    typedRecords.data[0];
+  Object.entries(keysToValueIndex).forEach(([key, index]) => {
+    if (values[index]) result[key] = values[index];
+  });
+  return schema.safeParse(result[recordsKeyToValidate]);
+};
+
 export const extractFirstErrorMessageFromSchemaError = (
   schemaErrorMessage: z.ZodError<any>["message"]
 ): string => {

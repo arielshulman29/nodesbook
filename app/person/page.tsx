7099b7e1243d57427420d6451b6ae4be4b52808f
arielshulman <ariel@illumex.ai>
2024-06-22 19:11:36 +0300

    fix: wrap search params in suspense

--- app/person/page.tsx
+++ app/person/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
 import PersonForm from "../_components/forms/PersonForm/PersonForm";
+import { Label } from "../_components/shared/styled";
 
 export default function PersonPage() {
-  return <PersonForm />;
+  return (
+    <Suspense fallback={<Label>Loading...</Label>}>
+      <PersonForm />
+    </Suspense>
+  );
 }
